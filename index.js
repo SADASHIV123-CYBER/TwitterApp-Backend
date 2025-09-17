ecf6611871d9276b02ba89958b665a5621b16bd2
sadashiv <sadashivkale604@gmail.com>
2025-09-17 10:45:02 +0530

    added cors

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import serverConfig from './src/config/serverConfig.js';
-// import connectDB from './src/config/dbConfig.js';
-import connectDB from './src/config/dbConfig.js'
+import connectDB from './src/config/dbConfig.js';
 import morgan from 'morgan';
-import apiRouter from './src/routes/apiRouter.js'
+import apiRouter from './src/routes/apiRouter.js';
 import cookieParser from 'cookie-parser';
+
 const app = express();
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.text())
-app.use(express.urlencoded( {extended: true} ));
-app.use(morgan('dev'))
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:5173', // your frontend URL
+    credentials: true,               // allow cookies
+}));
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.json({
         message: "pong"
-    })
-})
+    });
+});
 
 app.listen(serverConfig.PORT, async () => {
     await connectDB();
     console.log(`Server started at port ${serverConfig.PORT}`);
-    
-})
+});
