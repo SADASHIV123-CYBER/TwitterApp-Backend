04f167bf223f7ade46d28c2e23119a4134501671
sadashiv <sadashivkale604@gmail.com>
2025-09-18 00:12:51 +0530

    fixed issue in verify route

--- src/routes/v1/verify.js
+++ src/routes/v1/verify.js
@@ -3,7 +3,7 @@ import { isLoggedIn } from "../../middlewares/authMiddlewares.js";
 
 const verifyRouter = express.Router();
 
-router.get("/verify", isLoggedIn, (req, res) => {
+verifyRouter.get("/verify", isLoggedIn, (req, res) => {
   res.json({ success: true, user: req.user });
 });
 
