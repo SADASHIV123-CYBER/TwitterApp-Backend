4d3a5e63ee3d535486ea0161eb35f23e9402c087
sadashiv <sadashivkale604@gmail.com>
2025-09-17 23:41:03 +0530

    issue fixed

--- src/routes/v1/verify.js
+++ src/routes/v1/verify.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isLoggedIn } from "../../middlewares/authMiddlewares";
+import { isLoggedIn } from "../../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
