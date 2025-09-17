8ff3bc539d82b59bc9ecac02f24e1aa642e03b34
sadashiv <sadashivkale604@gmail.com>
2025-09-18 00:41:00 +0530

    cookie issue fixed

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,27 +1,53 @@
+// import { StatusCodes } from "http-status-codes";
+// import { loginUser } from "../service/authService.js";
+// import { errorResponce, successResponce } from "../utils/helpers/responses.js";
+// import logger from "../utils/helpers/logger.js";
+// // import NODE_ENV from '../config/serverConfig.js'
+// import serverConfig from "../config/serverConfig.js";
+
+// export async function login(req, res) {
+//     try {
+//         const loginPayload = req.body;
+
+//         const response = await loginUser(loginPayload);
+
+//         res.cookie("authToken", response, {
+//         httpOnly: true,
+//         secure: serverConfig.NODE_ENV === "production", // true on Render
+//         sameSite: "None", // allow cross-site cookies
+//         maxAge: 24 * 60 * 60 * 1000,
+//         });
+
+
+//         return successResponce(res, null, StatusCodes.OK, "logged in successfully");
+//     } catch (error) {
+//         logger.error(error);
+//         return errorResponce(res, error)
+//     }
+// }
+
 import { StatusCodes } from "http-status-codes";
 import { loginUser } from "../service/authService.js";
 import { errorResponce, successResponce } from "../utils/helpers/responses.js";
 import logger from "../utils/helpers/logger.js";
-// import NODE_ENV from '../config/serverConfig.js'
 import serverConfig from "../config/serverConfig.js";
 
 export async function login(req, res) {
     try {
         const loginPayload = req.body;
 
-        const response = await loginUser(loginPayload);
+        const token = await loginUser(loginPayload);
 
-        res.cookie("authToken", response, {
-        httpOnly: true,
-        secure: serverConfig.NODE_ENV === "production", // true on Render
-        sameSite: "None", // allow cross-site cookies
-        maxAge: 24 * 60 * 60 * 1000,
+        res.cookie("authToken", token, {
+            httpOnly: true,
+            secure: serverConfig.NODE_ENV === "production", // true only on Render/Prod
+            sameSite: "None", // IMPORTANT: required for cross-site cookies
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
-
 
         return successResponce(res, null, StatusCodes.OK, "logged in successfully");
     } catch (error) {
         logger.error(error);
-        return errorResponce(res, error)
+        return errorResponce(res, error);
     }
 }
